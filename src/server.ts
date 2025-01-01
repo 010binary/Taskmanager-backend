105e254ab9c8181dfdd2bf1418e99ad3b6470c82
010binary <augustine32fg@gmail.com>
2025-01-01 14:04:57 +0100

    Update .gitignore, add production logging middleware, and refactor server logging

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
-import { Response } from "express";
+import { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import logger from "@middleware/logger";
+import logger from "logs/logger";
+import pino from "@middleware/ProductionLogs";
 import router from "./router/";
 import "./register-paths";
 import swaggerUi from "swagger-ui-express"
@@ -35,14 +36,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 // Custom middleware
 app.use(logger);
+app.use(pino);
 
 // Routes
 app.use("/api/v1", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.get("/", (req, res: Response) => {
-  res.status(301).redirect("/api-docs");
+app.get("/", (_req: Request, res: Response) => {
+  res.redirect("/api-docs");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
