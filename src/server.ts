a0c60949f64f9622912505a43e5dd54e294232d3
010binary <augustine32fg@gmail.com>
2025-01-01 14:36:31 +0100

    Remove logger middleware and add a ping route for health checks

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import express from "express";
 import { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import logger from "logs/logger";
 import pino from "@middleware/ProductionLogs";
 import router from "./router/";
 import "./register-paths";
@@ -35,7 +34,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 // Custom middleware
-app.use(logger);
 app.use(pino);
 
 // Routes
@@ -43,6 +41,9 @@ app.use("/api/v1", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get("/", (_req: Request, res: Response) => {
   res.redirect("/api-docs");
+});
+app.get("/ping", (_req: Request, res: Response) => {
+  res.status(200).json({ message: "pong" });
 });
 
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
